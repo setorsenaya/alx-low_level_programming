77287edd37c9c56bcec83ab752128c44745c456e
Setor Senaya <senayasetor@gmail.com>
2022-03-16 12:14:57 +0000

    commit

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -15,6 +15,35 @@ int main(void)
         b = -98;
         c = 0;
 
+	/**
+ * largest_number - returns the largest of 3 numbers
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ * Return: largest number
+ */
+
+int largest_number(int a, int b, int c)
+{
+    int largest;
+
+    if (a > b && b > c)
+    {
+        largest = a;
+    }
+    else if (b > a && a > c)
+    {
+        largest = b;
+    }
+    else if (c > a && a > b)
+    {
+        largest = c;
+    }
+
+    return (largest);
+}
+
+
         largest = largest_number(a, b, c);
 
         printf("%d is the largest number\n", largest);
