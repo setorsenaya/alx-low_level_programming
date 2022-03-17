ff50367ab9e44183aee892fcb6d433268fbccab9
Setor Senaya <senayasetor@gmail.com>
2022-03-17 15:54:27 +0000

    commit

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,42 +1,11 @@
-int _putchar(char c);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
-void jack_bauer(void);
-void times_table(void);
-int add(int, int);
-void print_to_98(int n);
-void print_times_table(int n);
+#ifndef MAIN_H
+#define MAIN_H
 
-/**
- * largest_number - returns the largest of 3 numbers
- * @a: first integer
- * @b: second integer
- * @c: third integer
- * Return: largest number
- */
+#include <stdio.h>
 
-int largest_number(int a, int b, int c)
-{
-    int largest;
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
 
-    if (a > b && b > c)
-    {
-        largest = a;
-    }
-    else if (b > a && a > c)
-    {
-        largest = b;
-    }
-    else if (c > a && a > b)
-    {
-        largest = c;
-    }
-
-    return (largest);
-}
-
+#endif /* MAIN */
